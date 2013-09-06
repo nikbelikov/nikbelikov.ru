95b344331203328c914af04f27b7c2c44e1ebf90
nikbelikov <nikbelikov@me.com>
2013-09-06 22:13:59 +0400

    new design started

--- index.php
+++ index.php
@@ -1,17 +1,42 @@
-<?php
-/**
- * Front to the WordPress application. This file doesn't do anything, but loads
- * wp-blog-header.php which does and tells WordPress to load the theme.
- *
- * @package WordPress
- */
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+	<meta charset="utf-8" />
+	<title>Портфолио Николая Беликова</title>
+	<meta name="keywords" content="" />
+	<meta name="description" content="Портфолио Николая Беликова" />
 
-/**
- * Tells WordPress to load the WordPress theme and output it.
- *
- * @var bool
- */
-define('WP_USE_THEMES', true);
+	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=0.7">
 
-/** Loads the WordPress Environment and Template */
-require('./wp-blog-header.php');
+	<meta name="apple-mobile-web-app-capable" content="yes">
+	<meta name="apple-mobile-web-app-status-bar-style" content="black">
+	<meta name="apple-mobile-web-app-title" content="@_" />
+
+	<link rel="apple-touch-startup-image" href="startup-image.png" />
+	<link rel="apple-touch-startup-image" href="startup-image-iphone5.png" media="screen and (device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2)" />
+
+	<link href="favicon.ico" rel="shortcut icon" type="image/x-icon">
+	<link rel="apple-touch-icon-precomposed" href="apple-touch-icon-precomposed.png" />
+
+	<link rel="stylesheet" href="css/lib/font-awesome.min.css" type="text/css" media="screen, projection" />
+	<link href="http://fonts.googleapis.com/css?family=Ubuntu:300&amp;subset=latin,cyrillic" rel="stylesheet" type="text/css">
+	<link rel="stylesheet" href="css/style.css" type="text/css" media="screen, projection" />
+</head>
+
+<body>
+
+<section class="wrapper">
+	<ul class="social">
+		<li><a class="icon-twitter" href="#"></a></li>
+		<li><a class="icon-instagram" href="#"></a></li>
+		<li><a class="icon-tumblr" href="#"></a></li>
+		<li><a class="icon-github" href="#"></a></li>
+		<li><a class="icon-envelope-alt" href="#"></a></li>
+	</ul>
+	<footer>
+		<a href="http://nikbelikov.ru/" target="_blank"><?php echo date('Y'); ?> (c) nikbelikov.ru</a>
+	</footer>
+</section><!-- .wrapper -->
+
+</body>
+</html>
