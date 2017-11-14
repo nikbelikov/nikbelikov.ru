3433e77552c645102b190148b85903113da95ba5
Nikolay Belikov <nikmail88@gmail.com>
2017-11-14 13:56:10 +0400

    яндекс.метрика

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,15 @@ module.exports = {
   siteMetadata: {
     title: `nikbelikov.ru`,
   },
-  plugins: [`gatsby-plugin-react-helmet`],
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-plugin-yandex-metrika`,
+      options: {
+        trackingId: '46631805',
+        webvisor: true,
+        trackHash: true
+      },
+    },
+  ],
 };
