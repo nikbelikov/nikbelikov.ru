a146b9afced5919dc8483cd61ca2a1d2dc14dcbc
Nikolay Belikov <nikmail88@gmail.com>
2017-12-01 23:00:12 +0400

    обновил favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,11 +5,29 @@ module.exports = {
   plugins: [
     `gatsby-plugin-react-helmet`,
     {
+      resolve: `gatsby-plugin-favicon`,
+      options: {
+        logo: "./src/images/favicon.png",
+        injectHTML: true,
+        icons: {
+          android: true,
+          appleIcon: true,
+          appleStartup: true,
+          coast: false,
+          favicons: true,
+          firefox: true,
+          twitter: false,
+          yandex: false,
+          windows: false,
+        }
+      }
+    },
+    {
       resolve: `gatsby-plugin-yandex-metrika`,
       options: {
         trackingId: '46631805',
         webvisor: true,
-        trackHash: true
+        trackHash: true,
       },
     },
   ],
--- src/html.js
+++ src/html.js
@@ -1,7 +1,5 @@
-import React from "react";
-import favicon16x16 from './images/favicons/favicon-16x16.png';
-import favicon32x32 from './images/favicons/favicon-32x32.png';
-import appleTouchIcon from './images/favicons/apple-touch-icon.png';
+import React from 'react';
+import logoImage from './images/favicon.png';
 
 let stylesStr;
 if (process.env.NODE_ENV === `production`) {
@@ -33,14 +31,11 @@ module.exports = class HTML extends React.Component {
             content="width=device-width, initial-scale=1, shrink-to-fit=no"
           />
           <meta name="description" content="Frontend developer"/>
-          <link rel="apple-touch-icon" sizes="180x180" href={appleTouchIcon}/>
-          <link rel="icon" type="image/png" sizes="32x32" href={favicon32x32}/>
-          <link rel="icon" type="image/png" sizes="16x16" href={favicon16x16}/>
           <meta name="theme-color" content="#ffffff"/>
           <meta property="og:url" content="http://nikbelikov.ru/"/>
           <meta property="og:type" content="website"/>
           <meta property="og:title" content="nikbelikov.ru"/>
-          <meta property="og:image" content={appleTouchIcon}/>
+          <meta property="og:image" content={logoImage}/>
           <meta property="og:description" content="Frontend developer"/>
           <meta property="og:site_name" content="nikbelikov.ru"/>
           <meta name="apple-mobile-web-app-capable" content="yes"/>
